e2707981e3a9493eb1c4f0229be7498c5b99a8a5
carl <aixueer4ever@gmail.com>
2017-09-05 22:29:36 -0400

    nothing

--- comps/carvana/run.py
+++ comps/carvana/run.py
@@ -2,5 +2,7 @@
 def run_sol(flags):
     if flags.sol == "btb1":
         from comps.carvana.btb1.run import run
+    elif flags.sol == "carl":
+        from comps.carvana.carl.run import run
     run(flags)
 
