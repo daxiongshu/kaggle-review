4bfbcfabc6f6f04dd5e67738277f8c03a560a3dc
daxiongshu <aixueer4ever@gmail.com>
2017-09-09 15:41:30 -0400

    xgb rank

--- models/xgb_models/xgb_rank.py
+++ models/xgb_models/xgb_rank.py
@@ -9,7 +9,8 @@ class xgb_rank(object):
     def __init__(self,params):
         self.params = params
 
-    def fit(self,X,y,Xg,Xt=None,yt=None,Xgt=None):
+    def fit(self,X,y,Xg,Xt=None,yt=None,Xgt=None,load_model=None,save_model=None):
+        print(X.shape,y.shape)
         num_round = self.params['num_round']
         early_stopping_rounds = self.params['early_stopping_rounds']
         dtrain = xgb.DMatrix(X, y)
@@ -20,12 +21,14 @@ class xgb_rank(object):
             dvalid.set_group(Xgt)
             watchlist = [(dtrain, 'train'), (dvalid, 'valid')]
             bst = xgb.train(self.params, dtrain, num_round, evals = watchlist,
-                early_stopping_rounds=early_stopping_rounds,verbose_eval=1)
+                early_stopping_rounds=early_stopping_rounds,verbose_eval=1,xgb_model=load_model)
         else:
             watchlist = [(dtrain, 'train')]
             bst = xgb.train(self.params, dtrain, num_round, evals = watchlist,
-                verbose_eval=1)
+                verbose_eval=1,xgb_model=load_model)
         self.bst = bst
+        if save_model is not None:
+            bst.save_model(save_model)            
         
 
     def predict(self,Xt,Xg):
